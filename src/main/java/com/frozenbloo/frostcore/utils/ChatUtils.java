bfc12307bb6ec681a30e2d708ddc234a54d14046
Frozenbloo <61006165+Frozenbloo@users.noreply.github.com>
2023-02-04 16:32:19 +0000

    Text Centering

--- src/main/java/com/frozenbloo/frostcore/utils/ChatUtils.java
+++ src/main/java/com/frozenbloo/frostcore/utils/ChatUtils.java
@@ -27,4 +27,42 @@ public final class ChatUtils {
 
         return matcher.appendTail(buffer).toString();
     }
+
+    private final static int CENTER_PX = 154;
+
+    public static String CenteredMessage(String message){
+        String[] lines = ChatColor.translateAlternateColorCodes('&', message).split("\n", 40);
+        StringBuilder returnMessage = new StringBuilder();
+
+
+        for (String line : lines) {
+            int messagePxSize = 0;
+            boolean previousCode = false;
+            boolean isBold = false;
+
+            for (char c : line.toCharArray()) {
+                if (c == '§') {
+                    previousCode = true;
+                } else if (previousCode) {
+                    previousCode = false;
+                    isBold = c == 'l';
+                } else {
+                    DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
+                    messagePxSize = isBold ? messagePxSize + dFI.getBoldLength() : messagePxSize + dFI.getLength();
+                    messagePxSize++;
+                }
+            }
+            int toCompensate = CENTER_PX - messagePxSize / 2;
+            int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
+            int compensated = 0;
+            StringBuilder sb = new StringBuilder();
+            while(compensated < toCompensate){
+                sb.append(" ");
+                compensated += spaceLength;
+            }
+            returnMessage.append(sb.toString()).append(line).append("\n");
+        }
+
+        return returnMessage.toString();
+    }
 }
