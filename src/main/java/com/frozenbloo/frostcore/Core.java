f52e766d98a26f4540a6aa30788b2cd4a916b4a4
Frozenbloo <61006165+Frozenbloo@users.noreply.github.com>
2023-03-08 22:12:13 +0000

    Removed Obsolete code

--- src/main/java/com/frozenbloo/frostcore/Core.java
+++ src/main/java/com/frozenbloo/frostcore/Core.java
@@ -37,10 +37,6 @@ public final class Core extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new MOTDPingEvent(), this);
         getServer().getPluginManager().registerEvents(new WelcomeEvent(), this);
         //endregion
-        Bukkit.getServer().getConsoleSender().sendMessage("It is the life of the crystal, the architect of the flake, the fire of the frost, the soul of the sunbeam.");
-        Bukkit.getServer().getConsoleSender().sendMessage("This crisp winter air is full of it.");
-        Bukkit.getServer().getConsoleSender().sendMessage("All that is gold does not glitter, not all those who wander are lost;");
-        Bukkit.getServer().getConsoleSender().sendMessage("the old that is strong does not wither, deep roots are not reached by the frost.");
     }
 
     @Override
